f2c49e85f647de899690f40157808e7c4bb7647d
June Cheol Park <JackPark@Juneui-MacBook-Air.local>
2016-12-25 12:57:08 +0900

    upload study files and assignment no 1

---
+++ 04-if-statement.py
@@ -0,0 +1,20 @@
+# 참과 거짓 boolean
+# if
+# True , False
+# and, or, not
+
+a = True
+b = False
+# A is True or B is True
+print (a and b)
+# A is True and B is True
+print (a or b)
+
+
+d = 7
+if d > 10:
+    print("숫자 d는 5보다 크다")
+elif d>5 and d<= 10:
+    print("no. is same or less than 10, biggr than 5")
+else:
+    print ("숫자는 5보다 작거나 같습니다.")
---
+++ 07-functions.py
@@ -0,0 +1,20 @@
+# 함수 functions
+# 입력값 parameters, 변환값 return
+
+# 함수는 어떻게? def로 정의한다!
+
+def hello_friends(name):
+    print ("hello, {}".format(name))
+
+name1 = "jack"
+name2 = "jane"
+name3 = "marco"
+name4 = "tom"
+# print ("hello, {}".format(name2))
+# print ("hello, {}".format(name3))
+# print ("hello, {}".format(name4))
+
+hello_friends(name1)
+hello_friends(name2)
+hello_friends(name3)
+hello_friends(name4)
