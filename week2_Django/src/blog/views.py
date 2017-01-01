f479607be0e6802121188e8ba8eaf4f58d36dce6
j4ckp4rk <nartis87@gmail.com>
2017-01-02 01:53:59 +0900

    upload week2 lecture files
    week2: Django, Bootstrap, css, font awesome

--- week2_Django/src/blog/views.py
+++ week2_Django/src/blog/views.py
@@ -1,6 +1,24 @@
-from django.http import HttpResponse
-from django.shortcuts import render
 # Create your views here.
+# from django.http import HttpResponse
+from django.shortcuts import render
+# from random import randint
+from .models import Article
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the index.")
+    # random_number = randint(1,100)
+    ##
+    # return HttpResponse("Hello, world.U R @ index {}".format(random_number))
+    name = "JACK"
+    article_list = Article.objects.all()
+    # Article.objects.create(
+    #     title = "hello",
+    #     contents = "aaa",
+    #     view_count = 0,
+    # )
+    ctx = {
+        "article_list" : article_list,
+        "name" : name,
+    }
+
+    # return render(request, "index.html", { "name" : name })
+    return render(request, "index.html", ctx)
