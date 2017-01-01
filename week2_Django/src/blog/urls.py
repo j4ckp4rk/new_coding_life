f595781eaa88e962609788ea0278f06b5c3438f4
j4ckp4rk <nartis87@gmail.com>
2017-01-01 17:10:01 +0900

    save for checkout

--- week2_Django/src/blog/urls.py
+++ week2_Django/src/blog/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
-
 from . import views
+#from 에 .을 찍는 건 같은 폴더내에서 불러온다는 것
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
--- week2_Django/src/blog/views.py
+++ week2_Django/src/blog/views.py
@@ -1,6 +1,6 @@
-# Create your views here.
 from django.http import HttpResponse
 from django.shortcuts import render
+# Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the blog index.")
+    return HttpResponse("Hello, world. You're at the index.")
